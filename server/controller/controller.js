bb79a4f65f886da5f8607be920bb62da555033b7
Suneel Chauhan <suneelms31@gmail.com>
2022-03-29 11:39:34 +0530

    crud

---
+++ server/controller/controller.js
@@ -0,0 +1,93 @@
+var Userdb = require("../model/model");
+
+// create and save new user
+exports.create = (req, res) => {
+  // validate request
+  if (!req.body) {
+    res.status(400).send({ message: "Content can not be emtpy!" });
+    return;
+  }
+
+  // new user
+  const user = new Userdb({ 
+    name: req.body.name,
+    email: req.body.email,
+    gender: req.body.gender,
+    status: req.body.status,
+  });
+
+  // save user in the database
+  user
+    .save(user)
+    .then((data) => {
+      //res.send(data);
+      res.redirect('/add-user');
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message ||
+          "Some error occurred while creating a create operation",
+      });
+    });
+};
+
+// retrieve and return all users/ retrive and return a single user
+exports.find = (req, res) => {
+ 
+    if(req.query.id){
+        const id = req.query.id
+        Userdb.findById(id)
+        .then(user =>{
+            res.send(user)
+        })
+        .catch(err =>{
+            console.log(err);
+        })
+
+    }else{
+        Userdb.find()
+        .then((user) => {
+          res.send(user);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+};
+
+
+
+// Update a new idetified user by user id
+exports.update = (req, res)=>{
+  if(!req.body){
+      return res
+          .status(400)
+          .send({ message : "Data to update can not be empty"})
+  }
+
+  const id = req.params.id;
+  Userdb.findByIdAndUpdate(id, req.body, { useFindAndModify: false})
+      .then(data => {
+          if(!data){
+              res.status(404).send({ message : `Cannot Update user with ${id}. Maybe user not found!`})
+          }else{
+              res.send(data)
+          }
+      })
+      .catch(err =>{
+          res.status(500).send({ message : "Error Update user information"})
+      })
+}
+
+
+// delete a  user by user id
+exports.delete = (req, res) => {
+    const id = req.params.id;
+
+    Userdb.findByIdAndDelete(id)
+    .then(res.send({msg: 'user Deleted Successfully'}))
+    .catch(err =>{
+        console.log(err);
+    })
+};
